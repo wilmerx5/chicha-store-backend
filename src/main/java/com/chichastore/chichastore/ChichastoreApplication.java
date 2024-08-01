2eac38853a8d002fe024d1bc85d8e4883e59167b
wilmerx5 <wilmercampos2004@gmail.com>
2024-07-31 20:55:05 -0500

    creating project

---
+++ src/main/java/com/chichastore/chichastore/ChichastoreApplication.java
@@ -0,0 +1,13 @@
+package com.chichastore.chichastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChichastoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChichastoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chichastore/chichastore/ChichastoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chichastore.chichastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChichastoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
